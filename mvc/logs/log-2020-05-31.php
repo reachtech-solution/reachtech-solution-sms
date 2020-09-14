73e404a9e9f4d9faf90c6abeaea8bb2735170433
Enwemasor Barnabas <qhodeweb@gmail.com>
2020-09-14 06:20:39 +0100

    initial commit

---
+++ mvc/logs/log-2020-05-31.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-05-31 09:33:03 --> 404 Page Not Found: Install/purchase
+ERROR - 2020-05-31 16:45:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'school45' C:\Apache24\htdocs\school\main\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-05-31 16:45:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'school45' C:\Apache24\htdocs\school\main\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-05-31 16:45:01 --> Unable to connect to the database
+ERROR - 2020-05-31 16:45:01 --> Unable to connect to the database
+ERROR - 2020-05-31 20:57:26 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'school45' C:\Apache24\htdocs\school\main\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-05-31 20:57:27 --> Unable to connect to the database
+ERROR - 2020-05-31 21:00:30 --> Query error: Table 'school.video_courses' doesn't exist - Invalid query: SELECT *
+FROM `video_courses`
+ORDER BY `video_courses_id` desc
+ERROR - 2020-05-31 21:23:53 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\Apache24\htdocs\school\main\libraries\Email.php 1902
+ERROR - 2020-05-31 21:40:14 --> Query error: Table 'school.video_lessons' doesn't exist - Invalid query: SELECT *
+FROM `video_lessons`
+WHERE `video_courses_id` = '1'
+ORDER BY `video_lessons_id` desc
+ERROR - 2020-05-31 21:50:24 --> Could not find the language line "student_select_subject"
+ERROR - 2020-05-31 22:38:05 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\Apache24\htdocs\school\main\libraries\Email.php 1902
---
+++ mvc/logs/log-2020-06-01.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-06-01 10:24:16 --> Severity: Notice --> Undefined variable: videocourse C:\Apache24\htdocs\school\mvc\views\videocourse\editvideo.php 70
+ERROR - 2020-06-01 10:24:17 --> Severity: Notice --> Trying to get property 'video_course_id' of non-object C:\Apache24\htdocs\school\mvc\views\videocourse\editvideo.php 70
+ERROR - 2020-06-01 10:25:23 --> Severity: Notice --> Undefined variable: videocourse C:\Apache24\htdocs\school\mvc\views\videocourse\editvideo.php 74
+ERROR - 2020-06-01 10:28:37 --> Severity: Notice --> Undefined variable: videocourse C:\Apache24\htdocs\school\mvc\views\videocourse\editvideo.php 74
+ERROR - 2020-06-01 13:30:04 --> 404 Page Not Found: Videocourse/addvideo
+ERROR - 2020-06-01 15:17:12 --> Could not find the language line "edit_activitiescategory"
+ERROR - 2020-06-01 15:28:59 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\Apache24\htdocs\school\main\libraries\Email.php 1902
+ERROR - 2020-06-01 18:21:15 --> Severity: Notice --> Undefined offset: 0 C:\Apache24\htdocs\school\mvc\views\report\searchpaymentfees\SearchPaymentFeesReport.php 72
+ERROR - 2020-06-01 18:22:03 --> Severity: Notice --> Undefined offset: 0 C:\Apache24\htdocs\school\mvc\views\report\searchpaymentfees\SearchPaymentFeesReportPDF.php 22
